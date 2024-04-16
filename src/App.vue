d70065189ee6e1c755ff62d87347b541afd3b089
chirazzzz <chiragmehta.uk@gmail.com>
2024-04-16 09:03:24 +0100

    created Learning Resource and passed data via props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
-  <ul v-for="resource in storedResources" :key="resource">
-    <li>{{ resource }}</li>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
   </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
 export default {
+  components: {
+    LearningResource
+  },
   data() {
     return {
       storedResources: [
-        { id: 'offical-guide', title: 'Offical Guide', description: 'The offical Vue.js documentation', link: 'https://vuejs.org' },
-        { id: 'stack-overflow', title: 'Stack Overflow', description: 'The best Q&A site for computer programmers', link: 'https://stackoverflow.com/' },
-        { id: 'js-docs', title: 'MDN - JavaScript guide', description: 'Complete guide to JavaScript', link: 'https://developer.mozilla.org' },
+        {
+          id: 'offical-guide',
+          title: 'Offical Guide',
+          description: 'The offical Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'stack-overflow',
+          title: 'Stack Overflow',
+          description: 'The best Q&A site for computer programmers',
+          link: 'https://stackoverflow.com/'
+        },
+        {
+          id: 'js-docs',
+          title: 'MDN - JavaScript guide',
+          description: 'Complete guide to JavaScript',
+          link: 'https://developer.mozilla.org'
+        }
       ]
     }
-  },
+  }
 }
 </script>
