f601b4be7c15d915981df7f2d02ea7d7efd68638
chirazzzz <chiragmehta.uk@gmail.com>
2024-04-16 16:04:53 +0100

    Created AddResource form

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,49 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form @submit.prevent>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="4" />
+      </div>
+      <div class="form-control">
+        <label for="link">Title</label>
+        <input type="url" name="link" id="link" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15eem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1em 0;
+}
+</style>
